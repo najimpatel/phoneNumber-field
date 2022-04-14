db0b2f806c835af3ebe5deddb7116a1889fa2e65
najimpatel <59526180+najimpatel@users.noreply.github.com>
2022-04-14 18:42:43 +0530

    Update settings.py

--- phonetask/phonetask/settings.py
+++ phonetask/phonetask/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6dfwrp57(_m1w6l=u_mvknek0)4emq6lp3ah5puag6i=9zywu0'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
